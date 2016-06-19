10ec180acb4007a7b100902c6637fa12db1dc99b
Neurone <HShunter@126.com>
2016-06-19 22:59:00 +0800

    stop at 9.2

--- Ch09/regTrees.py
+++ Ch09/regTrees.py
@@ -14,6 +14,39 @@ def binSplitDataSet(dataSet, feature, value):
 	mat1 = dataSet[nonzero(dataSet[:,feature] <= value)[0],:][0]
 	return mat0, mat1
 
+def regLeaf(dataSet):
+	return mean(dataSet[:,-1])
+
+def regErr(dataSet):
+	return var(dataSet[:,-1]) * shape(dataSet)[0]
+
+def chooseBestSplit(dataSet, leafType = regLeaf, errType = regErr, ops = (1, 4)):
+	tolS = ops[0]; tolN = ops[1]
+	if len(set(dataSet[:,-1].T.tolist()[0])) == 1:
+		return None, leafType(dataSet)
+	m, n = shape(dataSet)
+	S = errType(dataSet)
+	beatS = inf; bestIndex = 0; bestValue = 0
+	for featIndex in range(n - 1):
+		featValues = [example[featIndex] for example in dataSet]
+		print featValues
+		uniqueVals = set(featValues)
+		for splitVal in uniqueVals:
+			print splitVal
+			mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+			if(shape(mat0)[0] < tolN) or (shape(mat1[0]) < tolN): continue
+			newS = errType(mat0) + errType(mat1)
+			if newS < bestS:
+				bestIndex = featIndex
+				bestValue = splitVal
+				bestS = newS
+	if (S - beatS) < tolS:
+		return None, leafType(dataSet)
+	mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+	if(shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+		return None, leafType(dataSet)
+	return bestIndex, bestValue
+
 def createTree(dataSet, leafType = regLeaf, errType = regErr, ops = (1, 4)):
 	feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
 	if feat == None: return val
