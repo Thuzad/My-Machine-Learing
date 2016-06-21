20f1ff04e00989cf7406d0b571c7ac27629aafd7
Neurone <HShunter@126.com>
2016-06-21 20:57:15 +0800

    chapter 10 finished

--- Ch09/regTrees.py
+++ Ch09/regTrees.py
@@ -66,8 +66,8 @@ def getMean(tree):
 	return (tree['left'] + tree['right']) / 2.0
 
 def prune(tree, testData):
-	if shape(teestData)[0] == 0: return getMean(tree)
-	if(isTree(tree['left']) or isTree(tree['left'])):
+	if shape(testData)[0] == 0: return getMean(tree)
+	if (isTree(tree['right']) or isTree(tree['left'])):
 		lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
 	if isTree(tree['left']): tree['left'] = prune(tree['left'], lSet)
 	if isTree(tree['right']): tree['right'] = prune(tree['right'], rSet)
@@ -81,4 +81,51 @@ def prune(tree, testData):
 			return treeMean
 		else: return tree
 	else: return tree
-		
+
+def linearSolve(dataSet):
+	m, n = shape(dataSet)
+	X = mat(ones((m ,n))); Y = mat(ones((m, 1)))
+	X[:,1:n] = dataSet[:,0:n-1]; Y = dataSet[:,-1]
+	xTx = X.T * X
+	if linalg.det(xTx) == 0.0:
+		raise NameError('This matrix is singular, cannot do inverse,\ntry increasing the second value of ops')
+	ws = xTx.I * (X.T * Y)
+	return ws, X, Y
+
+def modelLeaf(dataSet):
+	ws, X, Y = linearSolve(dataSet)
+	return ws
+
+def modelErr(dataSet):
+	ws, X, Y = linearSolve(dataSet)
+	yHat = X * ws
+	return sum(power(Y - yHat, 2))
+
+def regTreeEval(model, inDat):
+	return float(model)
+
+def modelTreeEval(model, inDat):
+	n = shape(inDat)[1]
+	X = mat(ones((1, n+1)))
+	X[:,1:n+1] = inDat
+	return float(X * model)
+
+def treeForeCast(tree, inData, modelEval = regTreeEval):
+	if not isTree(tree): return modelEval(tree, inData)
+	if inData[tree['spInd']] > tree['spVal']:
+		if isTree(tree['left']):
+			return treeForeCast(tree['left'], inData, modelEval)
+		else:
+			return modelEval(tree['left'], inData)
+	else:
+		if isTree(tree['right']):
+			return treeForeCast(tree['right'], inData, modelEval)
+		else:
+			return modelEval(tree['right'], inData)
+
+def createForeCast(tree, testData, modelEval = regTreeEval):
+	m = len(testData)
+	yHat = mat(zeros((m, 1)))
+	for i in range(m):
+		yHat[i , 0] = treeForeCast(tree, mat(testData[i]), modelEval)
+	return yHat
